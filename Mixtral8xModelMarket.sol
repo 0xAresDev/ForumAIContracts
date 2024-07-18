89fa69ab0a9970eee2da920bd494f8b036c0ea38
Ares <109671478+0xAresDev@users.noreply.github.com>
2024-07-18 15:26:06 +0200

    Update and rename LLMMarket.sol to Mixtral8xModelMarket.sol

--- Mixtral8xModelMarket.sol
+++ Mixtral8xModelMarket.sol
@@ -3,10 +3,12 @@ pragma solidity >=0.8.2 <0.9.0;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 
-contract Mixtral8x7BModelMarket{
+contract Mixtral8x7BMarket{
 
     address private _proofOfStakeContract;
-    //bool public paused;
+
+    event Request(address _host, uint256 _uniqueCode, uint256 _feesPaid);
+
 
     error Unauthorized(address account);
 
@@ -30,22 +32,16 @@ contract Mixtral8x7BModelMarket{
         uint256 price;
     }
 
-    struct Request{
-        uint256 uniqueCode;
-        uint256 feesPaid;
-    }
 
     Hoster[] public allHosts;
 
-    mapping(address host => Request[]) public activeRequests;
     mapping(address host => bool) public paused;
 
-    function addHost(string memory url, address account, uint256 price) external onlyProofOfStake returns (bool) {
+    function addHost(string memory url, address account, uint256 price) external onlyProofOfStake{
         allHosts.push(Hoster(url, account, price));
-        return true;
     }
 
-    function removeHost(address account) external onlyProofOfStake returns (bool) {
+    function removeHost(address account) external onlyProofOfStake{
         for(uint256 i=0; i<allHosts.length; i++){
             if(allHosts[i].hostAccount == account){
                 delete allHosts[i];
@@ -53,36 +49,29 @@ contract Mixtral8x7BModelMarket{
                     allHosts[u] = allHosts[u+1];
                 }
                 allHosts.pop();
-                return true;
             }
         }
-        return false;
     }
 
-    function changePrice(uint256 price) external returns (bool) {
+    function changePrice(uint256 price) external{
         for(uint256 i=0; i<allHosts.length; i++){
             if(allHosts[i].hostAccount == msg.sender){
                 allHosts[i].price = price;
-                return true;
             }
         }
-        return false;
     }
 
-    function pause() external returns (bool){
+    function pause() external{
         paused[msg.sender] = true;
-        return true;
     }
 
-    function unpause() external returns (bool){
+    function unpause() external{
         paused[msg.sender] = false;
-        return true;
     }
 
 
-    function pauseFromProofOfStake(address node) onlyProofOfStake external returns (bool) {
+    function pauseFromProofOfStake(address node) onlyProofOfStake external {
         paused[node] = true;
-        return true;
     }
 
 
@@ -96,18 +85,9 @@ contract Mixtral8x7BModelMarket{
         require(value >= 100, "Below minimum payment!");
         require(paymentToken.allowance(msg.sender, address(this))>=value, "Not enough allowance!");
         paymentToken.transferFrom(msg.sender, host, value);
-        activeRequests[host].push(Request(code, value));
-        return true;
+        emit Request(host, code, value); 
     }
 
-    function clearListAndReedemFunds() external returns (bool) {
-        delete activeRequests[msg.sender];
-        return true;
-    }
-
-    function getActiveRequests(address host) external view returns (Request[] memory) {
-        return activeRequests[host];
-    }
 
     function getPaused(address host) external view returns (bool){
         return paused[host];
